5898a3a9e0c97023458acbc0a5ce6747aa412910
jonghyunKo <skygg@naver.com>
2021-12-15 11:50:19 +0900

    타입스크립트 설정

--- typescript-practice/dist/hello.js
+++ typescript-practice/dist/hello.js
@@ -1,19 +1,17 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // 타입스크립트 시작하기
 //tsc hello.ts <- es5 문법으로 컴파일
 //tsc hello.ts --target es6 <- es6 문법으로 컴파일
 //tsc hello.ts --lib es5,es2015.promise, es2015.iterable, dom <-- es6에 관한 문법들을 예외 라이브러리로 등록
 //tsc.hello.ts --target es6 --lib es2015, dom --module commonjs <-- 실행모듈을 commonjs로 설정
 var hello = "hello";
-var hello2 = "hello";
+let hello2 = "hello";
 // @ts-ignore
-var timeoutPromise = new Promise(function (resolve, reject) {
-    setTimeout(function () {
+let timeoutPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
         resolve("1 sec");
     }, 1000);
 });
 timeoutPromise.then(console.log);
-var util_1 = require("./util");
-var value = (0, util_1.default)(1, 2);
+import add from './util.js';
+const value = add(1, 2);
 console.log(value);
--- typescript-practice/dist/util.js
+++ typescript-practice/dist/util.js
@@ -1,12 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.minus = void 0;
 //주어진 두 파라미터의 합을 구하는 함수
-function add(a, b) {
+export default function add(a, b) {
     return a + b;
 }
-exports.default = add;
-function minus(a, b) {
+export function minus(a, b) {
     return a - b;
 }
-exports.minus = minus;
