1d68d110932d3dedf41588890653f6d690a166bf
jonghyunKo <skygg@naver.com>
2021-12-15 11:37:57 +0900

    타입스크립트 설정파일 설정

--- typescript-practice/src/hello.ts
+++ typescript-practice/src/hello.ts
@@ -2,12 +2,17 @@
 //tsc hello.ts <- es5 문법으로 컴파일
 //tsc hello.ts --target es6 <- es6 문법으로 컴파일
 //tsc hello.ts --lib es5,es2015.promise, es2015.iterable, dom <-- es6에 관한 문법들을 예외 라이브러리로 등록
+//tsc.hello.ts --target es6 --lib es2015, dom --module commonjs <-- 실행모듈을 commonjs로 설정
 var hello = "hello";
 let hello2 = "hello";
 
+// @ts-ignore
 let timeoutPromise = new Promise((resolve, reject)=>{
     setTimeout(()=>{
         resolve("1 sec");
     }, 1000);
 });
 timeoutPromise.then(console.log);
+import add from './util';
+const value = add(1, 2);
+console.log(value);
