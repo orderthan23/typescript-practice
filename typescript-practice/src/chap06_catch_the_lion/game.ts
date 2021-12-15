e8c55341bef276118b063be7a5f3ec281babb516
jonghyunKo <skygg@naver.com>
2021-12-15 17:24:43 +0900

    사자잡기 게임 기본세팅

---
+++ typescript-practice/src/chap06_catch_the_lion/game.ts
@@ -0,0 +1,8 @@
+//name Export
+export class Game {
+
+}
+
+//default Export
+const version = 'v1';
+export default version
---
+++ typescript-practice/src/chap06_catch_the_lion/index.ts
@@ -0,0 +1,6 @@
+import version,{Game} from './game';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/style.css';
+
+
+new Game();
